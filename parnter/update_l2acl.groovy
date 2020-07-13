a480547aadb144e735b7a98e26b8e0dfbb9520f7
hsikai <hsikai.wang@ruckuswireless.com>
2020-07-13 06:00:09 +0000

    update update_l2acl

--- parnter/update_l2acl.groovy
+++ parnter/update_l2acl.groovy
@@ -10,7 +10,6 @@ pipeline {
         string(name: 'SCENARIO', defaultValue: 'phase1', description: '')
         string(name: 'VAR_DIR', defaultValue: '/var/lib/jenkins/api_perf/var/${scenario}', description: '')
         string(name: 'SZ_IP', defaultValue: '', description: '')
-        string(name: 'NPROC', defaultValue: '2', description: '')
         string(name: 'API_VERSION', defaultValue: '', description: '')
     }
 
@@ -46,6 +45,8 @@ echo "SZ_IP: $SZ_IP, SZ_NAME: $SZ_NAME, SZ_VERSION: $SZ_VERSION"
 ###
 
 domain_id=`head -1 $VAR_DIR/output/id/domain_ids.log | awk -F'|' '{print $1}'`
+tmp_json=$(mktemp l2acl-tmp-$SZ_IP-XXXXXXXXXX --tmpdir=/tmp)
+data_json=$(mktemp l2acl-data-$SZ_IP-XXXXXXXXXX --tmpdir=/tmp)
 
 
 ###
@@ -59,21 +60,26 @@ pubapi_login $SZ_USERNAME $SZ_PASSWORD
 query_all_l2acl_by_domain_id $domain_id > $VAR_DIR/output/id/l2acl_ids.log
 l2acl_id=`head -1 $VAR_DIR/output/id/l2acl_ids.log | awk -F'|' '{print \$1}'`
 
-get_l2acl_by_id $l2acl_id | sed -n 's/Response body: //p' | jq '.' > tmp.json
+get_l2acl_by_id $l2acl_id | sed -n 's/Response body: //p' | jq '.' > $tmp_json
 
+# remove attribute
 lines=(creatorId creatorUsername createDateTime modifierId modifierUsername modifiedDateTime domainId id)
 for l in ${lines[@]}; do
-  jq "del(.${l})" < tmp.json > result.json
-  cp result.json tmp.json
+  jq "del(.${l})" < $tmp_json > $data_json
+  cp $data_json $tmp_json
 done
-letter=`head /dev/urandom | tr -dc A-Za-z | head -c 8`
-jq ".description='"$letter"' tmp.json > result.json
 
-put_l2acl $l2acl_id result.json
+# update attribute
+letter=`head /dev/urandom | tr -dc A-Za-z | head -c 8`
+jq ".description=\\"\$letter\"" $tmp_json > $data_json
+
+put_l2acl $l2acl_id $data_json
 
 # logout
 pubapi_logout
 echo "end job:`date`"
+
+rm -fv $tmp_json $data_json
 '''
             }
         }
