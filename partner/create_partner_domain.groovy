335914cf5333607b616cd1bc757b0f67e10ddcfa
hsikai <hsikai.wang@ruckuswireless.com>
2020-07-20 06:58:43 +0000

    add per partner domain

---
+++ partner/create_partner_domain.groovy
@@ -0,0 +1,94 @@
+library identifier: 'dynamic-libary@master', retriever: modernSCM(
+        [$class: 'GitSCMSource',
+         remote: 'https://github.com/wsunccake/sztest_pipeline.git'])
+
+pipeline {
+    agent any
+    parameters {
+        string(name: 'SZ_VERSION', defaultValue: '1.0.0.0', description: '')
+        string(name: 'SCENARIO', defaultValue: '', description: '')
+        string(name: 'VAR_DIR', defaultValue: '/var/lib/jenkins/api_perf/var/${scenario}', description: '')
+        string(name: 'SZ_IP', defaultValue: '', description: '')
+        string(name: 'NPROC', defaultValue: '2', description: '')
+        string(name: 'API_VERSION', defaultValue: '', description: '')
+    }
+
+    stages {
+        stage('Update Build Name') {
+            steps {
+                script {
+                    currentBuild.displayName = "${SZ_VERSION} - ${SCENARIO} - #${currentBuild.number}"
+                }
+
+            }
+        }
+
+        stage('Create Partner Domain') {
+            steps {
+                sh '''#!/bin/bash
+###
+### setup var
+###
+
+SZTEST_HOME=/var/lib/jenkins/sztest
+source $SZTEST_HOME/conf/default/setup_var.sh
+source $SZTEST_HOME/util/api_util.sh
+source $SZTEST_HOME/util/test_api/partner.sh
+
+setup_api_var
+
+echo "SZ_IP: $SZ_IP, SZ_NAME: $SZ_NAME, SZ_VERSION: $SZ_VERSION"
+
+
+###
+### gen input
+###
+
+mkdir -p $VAR_DIR/output/partner_domains
+
+NEW_INPUT=domains.inp
+INPUT_NUMBER=1000
+TMP_DIR=`mktemp -d`
+echo "TMP DIR: $TMP_DIR"
+
+
+###
+### run api
+###
+
+for f in `ls $TMP_DIR/in_*`; do
+  # login
+  pubapi_login $SZ_USERNAME $SZ_PASSWORD
+  
+  cat $f | xargs -P $NPROC -i sh -c "create_partner_domain {} | tee $VAR_DIR/output/partner_domains/{}.out"
+  
+  # logout
+  pubapi_logout
+done
+echo "end job:`date`"
+
+rm -rfv $TMP_DIR
+'''
+            }
+        }
+
+        stage('Check Response') {
+            steps {
+                script {
+                    def result = util.checkResponseStatus "${VAR_DIR}/output/partner_domains"
+                    println result
+                    currentBuild.result = result
+                }
+            }
+        }
+
+        stage('Statistic Response') {
+            steps {
+                script {
+                    util.statisticizeResponse "${VAR_DIR}/output/partner_domains"
+                }
+            }
+        }
+
+    }
+}
