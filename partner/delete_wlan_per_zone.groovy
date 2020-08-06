60db54f7d8fe62aa0876e87978980f86d80296ad
wsunccake <wsunccake@hotmail.com>
2020-08-06 21:06:48 +0800

    delete wlan

---
+++ partner/delete_wlan_per_zone.groovy
@@ -0,0 +1,97 @@
+library identifier: 'dynamic-libary@master', retriever: modernSCM(
+        [$class: 'GitSCMSource',
+         remote: 'https://github.com/wsunccake/sztest_pipeline.git'])
+
+pipeline {
+    agent any
+    parameters {
+        string(name: 'SZ_VERSION', defaultValue: '1.0.0.0', description: '')
+        string(name: 'SCENARIO', defaultValue: '', description: '')
+        string(name: 'VAR_DIR', defaultValue: '/usr/share/nginx/html/api_perf/${SZ_VERSION}/${SCENARIO}', description: '')
+        string(name: 'SZ_IP', defaultValue: '', description: '')
+        string(name: 'NPROC', defaultValue: '2', description: '')
+        string(name: 'API_VERSION', defaultValue: '', description: '')
+    }
+
+    stages {
+        stage('Update Build Name') {
+            steps {
+                script {
+                    currentBuild.displayName = "${SZ_VERSION} - ${SCENARIO} - #${currentBuild.number}"
+                }
+
+            }
+        }
+
+        stage('Delete L2ACL Per Partner Domain') {
+            steps {
+                sh '''#!/bin/bash
+###
+### setup var
+###
+
+SZTEST_HOME=/var/lib/jenkins/sztest
+source $SZTEST_HOME/conf/default/setup_var.sh
+source $SZTEST_HOME/util/api_util.sh
+source $SZTEST_HOME/util/test_api/partner.sh
+
+setup_api_var
+
+echo "SZ_IP: $SZ_IP, SZ_NAME: $SZ_NAME, SZ_VERSION: $SZ_VERSION"
+
+
+###
+### gen input
+###
+
+ID_FILE=wlan_zone_domain.log
+INPUT_NUMBER=1000
+TMP_DIR=`mktemp -d`
+echo "TMP DIR: $TMP_DIR"
+
+mkdir -p $VAR_DIR/output/delete_wlan
+split -l $INPUT_NUMBER $VAR_DIR/output/id/$ID_FILE $TMP_DIR/in_
+
+
+###
+### run api
+###
+
+echo "start job:`date`"
+for f in `ls $TMP_DIR/in_*`; do
+  # login
+  pubapi_login $SZ_USERNAME $SZ_PASSWORD
+  
+  # delete wlan
+  sed 's/|/ /g' $f | xargs -n2 -P $NPROC sh -c 'delete_wlan ${0} ${2} | tee ${VAR_DIR}/output/delete_wlan/${0}.out'
+    
+  # logout
+  pubapi_logout
+done
+echo "end job:`date`"
+
+rm -rfv $TMP_DIR
+'''
+            }
+        }
+
+        stage('Check Response') {
+            steps {
+                script {
+                    def result = util.checkResponseStatus "${VAR_DIR}/output/delete_wlan", "204"
+                    println result
+                    currentBuild.result = result
+                }
+            }
+        }
+
+        stage('Statistic Response') {
+            steps {
+                script {
+                    util.statisticizeResponse "${VAR_DIR}/output/delete_wlan", "204"
+                }
+            }
+        }
+
+    }
+}
