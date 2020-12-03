bcbc5ce193c983c764ee088e50297032887ffaa8
kam <kam@a1.arrs.arrisi.com>
2020-12-04 04:56:35 +0800

    fix VAR_DIR

--- fr4818/create_zone.groovy
+++ fr4818/create_zone.groovy
@@ -7,7 +7,7 @@ pipeline {
     parameters {
         string(name: 'SZ_VERSION', defaultValue: '5.2.1.0.515', description: '')
         string(name: 'SCENARIO', defaultValue: 'fr4818', description: '')
-        string(name: 'VAR_DIR', defaultValue: '/var/lib/jenkins/api_perf/${SCENARIO}/${SZ_VERSION}', description: '')
+        string(name: 'VAR_DIR', defaultValue: '/usr/share/nginx/html/api_perf/${SZ_VERSION}/${SCENARIO}', description: '')
         string(name: 'SZ_IP', defaultValue: '', description: '')
         string(name: 'NPROC', defaultValue: '8', description: '')
         string(name: 'API_VERSION', defaultValue: 'v9_1', description: '')
